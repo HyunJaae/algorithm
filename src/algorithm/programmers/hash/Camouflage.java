3bd65644d73974dbc27f1228a4feb2b17c465f17
Devlee <hjcloud@me.com>
2022-07-01 23:48:54 +0900

    [programmers] 문제 - 위장 시작

---
+++ src/algorithm/programmers/hash/Camouflage.java
@@ -0,0 +1,15 @@
+package algorithm.programmers.hash;
+
+public class Camouflage {
+
+    public static int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String[][] participant = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+
+        System.out.println(solution(participant));
+    }
+}
