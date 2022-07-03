b0783562960cbeda03f48b635856ea5396d6215e
Devlee <hjcloud@me.com>
2022-07-03 15:17:48 +0900

    [programmers] 위장 문제 실패

--- src/algorithm/programmers/hash/Camouflage.java
+++ src/algorithm/programmers/hash/Camouflage.java
@@ -1,17 +1,50 @@
 package algorithm.programmers.hash;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
+// TODO: 실패
 public class Camouflage {
 
     public static int solution(String[][] clothes) {
         int answer = 0;
-        String first = clothes[0][0];
-        System.out.println(first);
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            String type = clothes[i][1];
+            hm.put(type, hm.getOrDefault(type, 0));
+        }
+        // 1개씩 + 2개씩 + 3개씩 ... > 안겹칠 때
+        // 1개씩 + 2개씩 -1 + 3개씩 -(n-2) + 4개씩 -(n-2) ... n개씩 -(n-2) > 겹치는게 2개 있을 때 = t = 2
+        // 1개씩 + 2개씩 -()
+        answer = hm.size();
         return answer;
     }
 
-    public static void main(String[] args) {
-        String[][] participant = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+    public static int solution01(String[][] clothes) {
+        // 1. 옷을 종류별로 구분하기
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String[] clothe : clothes) {
+            String type = clothe[1];
+            map.put(type, map.getOrDefault(type, 0) + 1);
+        }
 
-        System.out.println(solution(participant));
+        // 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
+        Iterator<Integer> it = map.values().iterator();
+        int answer = 1;
+
+        while(it.hasNext())
+            answer *= it.next().intValue() + 1;
+
+        // 3. 아무종류의 옷도 입지 않는 경우 제외하기
+        return answer - 1;
+    }
+
+    public static void main(String[] args) {
+        String[][] spy = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        String[][] spy00 = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
+        System.out.println(solution01(spy));
+        System.out.println(solution01(spy00));
     }
 }
