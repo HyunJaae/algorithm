72e967ce02993884ad05bc95761b78e754bf43cb
Devlee <hjcloud@me.com>
2022-07-24 20:39:45 +0900

    [programmers] Camouflage retry

--- src/algorithm/programmers/hash/Camouflage.java
+++ src/algorithm/programmers/hash/Camouflage.java
@@ -1,51 +1,62 @@
 package algorithm.programmers.hash;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 // TODO: 실패
 public class Camouflage {
 
     public static int solution(String[][] clothes) {
         int answer = 0;
-        HashMap<String, Integer> hm = new HashMap<>();
-
-        for (int i = 0; i < clothes.length; i++) {
-            String type = clothes[i][1];
-            hm.put(type, hm.getOrDefault(type, 0));
-        }
-        // 1개씩 + 2개씩 + 3개씩 ... > 안겹칠 때
-        // 1개씩 + 2개씩 -1 + 3개씩 -(n-2) + 4개씩 -(n-2) ... n개씩 -(n-2) > 겹치는게 2개 있을 때 = t = 2
-        // 1개씩 + 2개씩 -()
-        answer = hm.size();
-        return answer;
-    }
-
-    public static int solution01(String[][] clothes) {
-        // 1. 옷을 종류별로 구분하기
         HashMap<String, Integer> map = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+
         for (String[] clothe : clothes) {
             String type = clothe[1];
             map.put(type, map.getOrDefault(type, 0) + 1);
-            System.out.println("HashMap 값: " + map);
+        }
+        System.out.println(map);
+
+        int allSum = 0;
+        int allMul = 0;
+        int mulNum = 0;
+
+        for (String key : map.keySet()) {
+            allSum += map.get(key);
+            arr.add(map.get(key));
         }
 
-        // 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
-        Iterator<Integer> it = map.values().iterator();
-        int answer = 1;
+        if(arr.size() > 1) {
+            allMul = 1;
+            for (int i = 0; i < arr.size(); i++) {
+                allMul *= arr.get(i);
+            }
+        }
 
-        while (it.hasNext())
-            answer *= it.next() + 1;
+        if(arr.size() > 2) {
+            for (int i = 0; i < arr.size(); i++) {
+                for (int j = i + 1; j < arr.size(); j++) {
+                    mulNum += arr.get(i) * arr.get(j);
+                }
+            }
+        }
 
-        // 3. 아무종류의 옷도 입지 않는 경우 제외하기
-        return answer - 1;
+
+        System.out.println("모두 더한 값: " + allSum + "\n모두 곱한 값: " + allMul + "\n각자 곱한 값: " + mulNum);
+
+        answer = allSum + allMul + mulNum;
+
+        return answer;
     }
 
     public static void main(String[] args) {
-        String[][] spy = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        String[][] spy = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"},
+                {"red_hat", "headgear"}, {"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"white_sunglasses", "eyewear"}};
         String[][] spy00 = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+        String[][] spy01 = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
 
-        System.out.println(solution01(spy));
-        System.out.println(solution01(spy00));
+        System.out.println(solution(spy));
+        System.out.println(solution(spy00));
+        System.out.println(solution(spy01));
     }
 }
