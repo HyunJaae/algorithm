8ed577278da6f14088e76bc82963b48d2e6df2ed
Devlee <hjcloud@me.com>
2022-09-03 19:46:07 +0900

    [programmers] DiskController start

---
+++ src/algorithm/programmers/heap/DiskController.java
@@ -0,0 +1,16 @@
+package algorithm.programmers.heap;
+
+public class DiskController {
+    public int solution(int[][] jobs) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        DiskController diskController = new DiskController();
+        int[][] jobs = {{0, 3}, {1, 9}, {2, 6}};
+
+        System.out.println(diskController.solution(jobs));
+    }
+}
