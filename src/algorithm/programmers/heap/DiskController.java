96e085f33d0739f8566bb8232960432adc236745
Devlee <hjcloud@me.com>
2022-09-11 16:36:22 +0900

    [programmers] try07

--- src/algorithm/programmers/heap/DiskController.java
+++ src/algorithm/programmers/heap/DiskController.java
@@ -4,19 +4,7 @@ import java.util.*;
 
 public class DiskController {
     public int solution(int[][] jobs) {
-        int answer = jobs[0][1];
-
-        Arrays.sort(jobs, Comparator.comparingInt(o1 -> o1[0]));
-
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-        for (int i = 0; i < jobs.length; i++) {
-            for (int j = i + 1; j < jobs.length; j++) {
-                if (jobs[i][1] > jobs[j][0]) {
-
-                }
-            }
-        }
+        int answer = 0;
 
 
 
