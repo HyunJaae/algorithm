680b88ed2ffcde8e2f320d1d4845fd25691dcf86
Devlee <hjcloud@me.com>
2023-02-24 06:23:01 +0900

    B1000: A+B

---
+++ src/com/algorithm/baekjoon/B1000.java
@@ -0,0 +1,12 @@
+package com.algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class B1000 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+}
