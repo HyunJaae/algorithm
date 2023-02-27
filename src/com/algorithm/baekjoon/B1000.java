7b97dbe2d5f486c21805b5b96a5d2ff3dfb37696
Devlee <hjcloud@me.com>
2023-02-28 06:31:05 +0900

    B1000: A+B

--- src/com/algorithm/baekjoon/B1000.java
+++ src/com/algorithm/baekjoon/B1000.java
@@ -1,12 +1,39 @@
 package com.algorithm.baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/**
+ * A와 B 값을 입력 받아 더해보자
+ * @author devlee
+ */
 public class B1000 {
 
-    public static void main(String[] args) {
+    /**
+     * Scanner 를 이용한 입력
+     * @param args
+     */
+    public static void main00(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(sc.nextInt() + sc.nextInt());
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + b);
+
+        sc.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(a+b);
     }
 }
