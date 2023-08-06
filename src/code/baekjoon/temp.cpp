4bebfc50cd7dcf80ecc97f2640f3921d6d72a429
Devlee <hjcloud@me.com>
2023-08-07 00:48:29 +0900

    입출력과 사칙연산 끝

---
+++ src/code/baekjoon/temp.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long int a, b, c;
+int main() {
+    cout << "|\\_/|" << '\n';
+    cout << "|q p|   /}" << '\n';
+    cout << "( 0 )\"\"\"\\" << '\n';
+    cout << "|\"^\"`    |" << '\n';
+    cout << "||_/=\\\\__|" << '\n';
+    return 0;
+}
+
+// |\_/|
+// |q p|   /}
+// ( 0 )"""\
+// |"^"`    |
+// ||_/=\\__|
