7aba15caaa2efc37895a278b64193be4809bb432
Devlee <hjcloud@me.com>
2023-08-13 16:20:38 +0900

    baekjoon - 10871

--- src/code/baekjoon/temp.cpp
+++ src/code/baekjoon/temp.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
-int A, B;
+int N, X;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
- 
-    while(cin >> A >> B) {
-        cout << A + B << '\n';
+    
+    cin >> N >> X;
+    int arr[N];
+    for(int i = 0; i < N; i++) {
+        int arrVal;
+        cin >> arrVal;
+        arr[i] = arrVal;
     }
+    for(int i = 0; i < N; i++) {
+        if(arr[i] < X) cout << arr[i] << '\n';
+    }
+
     return 0;
 }
 
