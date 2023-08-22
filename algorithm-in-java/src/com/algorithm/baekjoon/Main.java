48665fc2a105a10eacf183f3aeec74efb192d266
Devlee <hjcloud@me.com>
2023-08-22 22:50:09 +0900

    B2562: list 로 풀기

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -3,20 +3,21 @@ package com.algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int max = 0;
-        int inputIndex = 0;
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
             int input = Integer.parseInt(br.readLine());
-            if (input > max) {
-                max = input;
-                inputIndex = i + 1;
-            }
+            list.add(input);
         }
+        int max = Collections.max(list);
+        int inputIndex = list.indexOf(max) + 1;
         System.out.println(max + "\n" + inputIndex);
     }
 }
