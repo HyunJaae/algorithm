53661ddc5b5731cc80019dbca9d2b42d8b313e90
Devlee <hjcloud@me.com>
2023-08-22 22:47:26 +0900

    B2562: 배열 없이 최대값과 인덱스 구하기

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -3,22 +3,20 @@ package com.algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int min = 1000001;
-        int max = -1000001;
-        for (int i = 0; i < N; i++) {
-            int input = Integer.parseInt(st.nextToken());
-            if (input < min) min = input;
-            if (input > max) max = input;
+        int max = 0;
+        int inputIndex = 0;
+        for (int i = 0; i < 9; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input > max) {
+                max = input;
+                inputIndex = i + 1;
+            }
         }
-
-        System.out.println(min + " " + max);
+        System.out.println(max + "\n" + inputIndex);
     }
 }
