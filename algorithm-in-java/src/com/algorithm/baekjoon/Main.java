fc84d01f52139300d2ad71ac51666e402abbf288
Devlee <hjcloud@me.com>
2023-08-24 19:17:24 +0900

    B10810: hashMap 공 넣기

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -3,22 +3,32 @@ package com.algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            int input = Integer.parseInt(br.readLine());
-            list.add(input);
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer, Integer> bucket = IntStream.range(0, n).boxed().collect(Collectors.toMap(i -> i + 1, i -> 0, (a, b) -> b, HashMap::new));
+
+        for(int s = 0; s < m; s++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            IntStream.rangeClosed(i, j).forEach(t -> bucket.put(t, k));
         }
-        int max = Collections.max(list);
-        int inputIndex = list.indexOf(max) + 1;
-        System.out.println(max);
-        System.out.println(inputIndex);
+        String sb = bucket.values().stream().mapToInt(value -> value).mapToObj(value -> value + " ").collect(Collectors.joining());
+
+        System.out.println(sb);
+
     }
 }
