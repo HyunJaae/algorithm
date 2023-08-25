5c24766b9f2f5bc53f20e3c2ff328d84c96726eb
Devlee <hjcloud@me.com>
2023-08-25 21:05:14 +0900

    B10813: BufferedWriter 쓰기, 20ms 단축

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -1,14 +1,13 @@
 package com.algorithm.baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
@@ -29,7 +28,9 @@ public class Main {
         }
 
         for (int a : arr) {
-            System.out.printf("%d ", a);
+            bw.write(a + " ");
         }
+
+        bw.flush();
     }
 }
