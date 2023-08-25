3acb5efbfc1a9dbb43d600bbf672991b515c6f34
Devlee <hjcloud@me.com>
2023-08-25 20:56:04 +0900

    B10813: 공바꾸기 stream 안쓰기 - 메모리 사용량 1800kb 감소

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -3,7 +3,6 @@ package com.algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,7 +14,9 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
-        Arrays.setAll(arr, i -> i + 1);
+        for (int i = 0; i < n; i++) {
+            arr[i] = i + 1;
+        }
 
         for (int q = 0; q < m; q++) {
             st = new StringTokenizer(br.readLine(), " ");
@@ -23,10 +24,12 @@ public class Main {
             int j = Integer.parseInt(st.nextToken());
 
             int iArr = arr[i - 1];
-            int jArr = arr[j - 1];
-            arr[i - 1] = jArr;
+            arr[i - 1] = arr[j - 1];
             arr[j - 1] = iArr;
         }
-        Arrays.stream(arr).mapToObj(a -> a + " ").forEach(System.out::printf);
+
+        for (int a : arr) {
+            System.out.printf("%d ", a);
+        }
     }
 }
