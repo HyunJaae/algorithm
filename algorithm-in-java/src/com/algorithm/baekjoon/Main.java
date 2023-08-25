671cc24463246251bee022bb4b12758f032b036b
Devlee <hjcloud@me.com>
2023-08-25 20:48:06 +0900

    B10813: 공바꾸기

--- algorithm-in-java/src/com/algorithm/baekjoon/Main.java
+++ algorithm-in-java/src/com/algorithm/baekjoon/Main.java
@@ -3,6 +3,7 @@ package com.algorithm.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,24 +15,18 @@ public class Main {
         int m = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[n];
+        Arrays.setAll(arr, i -> i + 1);
 
         for (int q = 0; q < m; q++) {
             st = new StringTokenizer(br.readLine(), " ");
             int i = Integer.parseInt(st.nextToken());
             int j = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
 
-            for (int w = i - 1; w < j; w++) {
-                arr[w] = k;
-            }
+            int iArr = arr[i - 1];
+            int jArr = arr[j - 1];
+            arr[i - 1] = jArr;
+            arr[j - 1] = iArr;
         }
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int v : arr) {
-            sb.append(v).append(" ");
-        }
-
-        System.out.println(sb);
+        Arrays.stream(arr).mapToObj(a -> a + " ").forEach(System.out::printf);
     }
 }
