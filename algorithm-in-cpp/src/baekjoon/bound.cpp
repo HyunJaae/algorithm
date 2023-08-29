65cb8cc6e80352924284bc4ef4fbdc0ca11b3af2
Devlee <hjcloud@me.com>
2023-08-29 23:51:05 +0900

    upper_bound(), lower_bound()

---
+++ algorithm-in-cpp/src/baekjoon/bound.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> v;
+int main() {
+    for(int i = 2; i <= 5; i++) v.push_back(i);
+    v.push_back(7);
+    // 2, 3, 4, 5, 7
+    cout << upper_bound(v.begin(), v.end(), 6) - v.begin() << '\n';
+    
+    return 0;
+}
--- algorithm-in-cpp/src/concept/memory.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int i;
-int main() {
-    cout << &i << '\n';
-    i = 0;
-    cout << &i << '\n';
-    return 0;
-}
