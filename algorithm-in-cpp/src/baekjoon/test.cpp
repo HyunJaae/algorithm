dd04f55c149a0a4fa74c32332e36808523092590
Devlee <hjcloud@me.com>
2023-09-12 23:02:43 +0900

    B2309: permutation and combination

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -1,32 +1,51 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int arr[101];
-int n = 0;
-int m = 0;
-int i, j, temp;
+void print(vector<int> b) {
+    sort(b.begin(), b.end());
+    for(int i : b) cout << i << ' ';
+    cout << '\n';
+}
+
+int arr[9];
+vector<int> result;
+int n = 9, r = 7;
+void combi(int start, vector<int> b) {
+    if(b.size() == r) {
+        int sum = 0;
+        for (int i : b) sum += i;
+        sort(b.begin(), b.end());
+        if (sum == 100) {
+            for (int i : b) cout << i << '\n';
+            exit(0);
+        }
+        result = b;
+        return;
+    }
+
+    for (int i = start + 1; i < n; i++) {
+        b.push_back(arr[i]);
+        combi(i, b);
+        b.pop_back();
+    }
+    return;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
 
-    cin >> n >> m;
-
-    for(int o = 0; o < n; o++) {
-        arr[o] = o + 1;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
-
-    for(int o = 0; o < m; o++) {
-        cin >> i >> j;
-        temp = arr[i - 1];
-        arr[i - 1] = arr[j -1];
-        arr[j - 1] = temp;
-    }
-
-    for(int o = 0; o < n; o++) {
-        cout << arr[o] << ' ';
-    }
-    cout << '\n';
+    
+    vector<int> b;
+    combi(-1, b);
+    for (int i : result) cout << i << '\n';
 
     return 0;
 }
