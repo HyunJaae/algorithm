65f11ab807f16e037576b1dfe2bb23019baa03a6
Devlee <hjcloud@me.com>
2023-09-21 14:19:22 +0900

    B1159: 다른 사람 풀이

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -1,31 +1,18 @@
 #include <iostream>
-#include <map>
 using namespace std;
 
-map<char, int> m;
-int N;
-string firstName;
+int N, cnt[26];
+string s, ret;
 int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> N;
-    for(int i = 0; i < N; i++) {
-        cin >> firstName;
-        char ff = firstName.front();
-        if (m.find(ff) != m.end()) {
-            m[ff]++;
-            continue;
-        }
-        m[ff] = 1;
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        cnt[s[0] - 'a']++;
     }
-    bool check = true;
-    for (auto v : m) {
-        if (v.second >= 5) {
-            cout << v.first;
-            check = false;
-        }
-    }
-    if (check) cout << "PREDAJA";
-    cout << '\n';
-
+    for (int i = 0; i < 26; i++) if(cnt[i] >= 5) ret += i + 'a';
+    if(ret.size()) cout << ret << '\n';
+    else cout << "PREDAJA" << '\n';
     return 0;
 }
 
