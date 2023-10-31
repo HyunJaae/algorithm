04ba2a5b23b3502b0f27bbc6a63ce7e49479887d
Devlee <hjcloud@me.com>
2023-10-31 22:26:09 +0900

    B10988: 팰린드롬인지 확인하기 완료

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -1,18 +1,15 @@
+#include <algorithm>
 #include <iostream>
+#include <string>
 using namespace std;
 
-int N, cnt[26];
-string s, ret;
+string s, temp;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> s;
-        cnt[s[0] - 'a']++;
-    }
-    for (int i = 0; i < 26; i++) if(cnt[i] >= 5) ret += i + 'a';
-    if(ret.size()) cout << ret << '\n';
-    else cout << "PREDAJA" << '\n';
+    cin >> s;
+    temp = s;
+    reverse(temp.begin(), temp.end());
+    cout << (temp == s) << '\n';
     return 0;
 }
 
