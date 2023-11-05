1072451247188903736aa61190b8b3e7891fc0b3
Devlee <hjcloud@me.com>
2023-11-05 17:29:06 +0900

    B9996: 반례 -> pattern: ab*ba fileName: aba = NE 가 출력되어야 하나 DA 가 출력되는 코드

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -2,25 +2,29 @@
 #include <string>
 using namespace std;
 
-string s;
+int N;
+string pattern, fileName, first, last;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    getline(cin, s);
-    
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] >= 65 && s[i] < 91) {
-            if (s[i] + 13 > 90) s[i] = s[i] + 13 - 26;
-            else s[i] = s[i] + 13;
+    cin >> N >> pattern;
+    int pos = pattern.find('*');
+    first = pattern.substr(0, pos);
+    last = pattern.substr(pos + 1);
+    for (int i = 0; i < N; i++) {
+        cin >> fileName;
+        // if (fileName.size() < first.size() + last.size()) cout << "NE\n";
+        if (fileName.size() < pos || fileName.size() < last.size()) {
+            cout << "NE\n";
+        } else {
+            if (fileName.substr(0, pos) == first && fileName.substr(fileName.size() - last.size()) == last) cout << "DA\n";
+            else cout << "NE\n";
         }
-        if (s[i] >= 97 && s[i] < 123) {
-            if (s[i] + 13 > 122) s[i] = s[i] + 13 - 26;
-            else s[i] = s[i] + 13;
-        }
+        
     }
-    cout << s << '\n';
-
     return 0;
 }
+
+
 
 // |\_/|
 // |q p|   /}
