24a72e8e6f19151bea40b81de4c78fbfceca23f0
devhyun <hjcloud@me.com>
2023-11-27 13:31:04 +0900

    B1620: 참고 풀이

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 #include <string>
+#include <map>
 using namespace std;
 
-int N;
-string pattern, pre, las, fileName;
+int N, M;
+string s, pNames[100004];
+map<string, int> m;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N >> pattern;
-    int pos = pattern.find('*');
-    pre = pattern.substr(0, pos);
-    las = pattern.substr(pos + 1);
-    cout << las.size() << ' ' << pos+1 << '\n';
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        cin >> s;
+        pNames[i + 1] = s;
+        m[s] = i + 1;
+    }
+
+    for (int i = 0; i < M; i++) {
+        cin >> s;
+        if (atoi(s.c_str()) == 0) cout << m[s] << '\n';
+        else cout << pNames[atoi(s.c_str())] << '\n';
+    }
     return 0;
 }
-
-
-
-// |\_/|
-// |q p|   /}
-// ( 0 )"""\
-// |"^"`    |
-// ||_/=\\__|
