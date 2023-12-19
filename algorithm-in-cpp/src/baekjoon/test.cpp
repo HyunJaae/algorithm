8753df300ff9f921882cfedb9b7a8f8f9f57569c
devhyun <hjcloud@me.com>
2023-12-19 10:37:11 +0900

    B2559: 풀이 완료(누적합 개념 부족)

--- algorithm-in-cpp/src/baekjoon/test.cpp
+++ algorithm-in-cpp/src/baekjoon/test.cpp
@@ -1,24 +1,20 @@
 #include <iostream>
-#include <string>
-#include <map>
 using namespace std;
 
-int N, M;
-string s, pNames[100004];
-map<string, int> m;
+int N, K, temp, psum[100001], ret = -1000000;
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> N >> M;
-    for (int i = 0; i < N; i++) {
-        cin >> s;
-        pNames[i + 1] = s;
-        m[s] = i + 1;
+    cin >> N >> K;
+    for(int i = 1; i <= N; i++) {
+        cin >> temp;
+        psum[i] = psum[i - 1] + temp;
     }
+    for(int i = K; i <= N; i++) {
+        cout << psum[i] - psum[i - K] << ' ';
+        ret = max(ret, psum[i] - psum[i - K]);
+    }
+    cout << '\n';
 
-    for (int i = 0; i < M; i++) {
-        cin >> s;
-        if (atoi(s.c_str()) == 0) cout << m[s] << '\n';
-        else cout << pNames[atoi(s.c_str())] << '\n';
-    }
+    cout << ret << '\n';
     return 0;
 }
