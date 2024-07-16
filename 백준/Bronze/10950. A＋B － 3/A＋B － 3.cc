505aa241ae3d5e4b174128001cba09bb7a39c536
Lee <hjcloud@me.com>
2024-07-16 11:39:42 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int T, A, B;
+int main() {
+    ios_base::sync_with_stdio(false);
+
+    cin >> T;
+
+    for(int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << A + B << '\n';
+    }
+
+    return 0;
+}
