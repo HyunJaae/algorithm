eab83172f8c727240b25851b83b593b0a29773c6
Lee <hjcloud@me.com>
2024-07-16 12:21:31 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int T, A, B;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    cin >> T;
+
+    for(int i = 0; i < T; i++) {
+        cin >> A >> B;
+        cout << "Case #" << i + 1 << ": " << A + B << '\n';
+    }
+    return 0;
+}
