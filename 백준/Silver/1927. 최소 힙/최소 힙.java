91e718131edd7413e378e801f76a277158105a06
Lee <hjcloud@me.com>
2024-07-16 11:20:09 +0900

    [Silver II] Title: 최소 힙, Time: 328 ms, Memory: 26240 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> heep = new PriorityQueue<>();
+        for (int i =0; i < T; i++) {
+            int X = Integer.parseInt(br.readLine());
+
+            if(X != 0) {
+                heep.add(X);
+            } else if(heep.isEmpty()) {
+                sb.append(0 + "\n");
+            }
+            else {
+                sb.append(heep.poll()).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
