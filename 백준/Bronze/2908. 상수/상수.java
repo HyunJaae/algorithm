3e30c126d86161d67f14ba71f2d2f8d4ac4cefe4
Lee <hjcloud@me.com>
2024-07-16 11:30:24 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int A=0;
+        int B=0;
+
+        A+=System.in.read()-48;
+        A+=(System.in.read()-48)*10;
+        A+=(System.in.read()-48)*100;
+
+        System.in.read();
+
+        B+=System.in.read()-48;
+        B+=(System.in.read()-48)*10;
+        B+=(System.in.read()-48)*100;
+
+        System.out.println(Math.max(A, B));
+    }
+}
