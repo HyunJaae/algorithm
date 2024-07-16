38b3c5798f6f9deaae7c7606d8c6adbdfa54deb0
Lee <hjcloud@me.com>
2024-07-16 12:23:07 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        int ch = System.in.read();
+        System.out.println(ch);
+    }
+}
