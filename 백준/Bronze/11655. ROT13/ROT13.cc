05c27c8e5b7eaccf7d065a1fc842e52dc0223b8d
Lee <hjcloud@me.com>
2024-07-16 12:23:18 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string s, temp;
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    getline(cin, s);
+    for (char c : s) {
+        if (c < 58 || c == ' ') {
+            temp += c;
+            continue;
+        }
+        if (c - 'a' < 0) {
+            if ((c-'A') + 13 >= 26) {
+                temp += c+13-26;
+            } else temp += c+13;
+        } else {
+            if ((c - 'a') + 13 >= 26) {
+                temp += c+13-26;
+            } else temp += c+13;
+        }
+    }
+    cout << temp << '\n';
+    return 0;
+}
