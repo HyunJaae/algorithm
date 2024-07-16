39ef2e98bf1439c52c690b463850481d36e11ed1
Lee <hjcloud@me.com>
2024-07-16 11:38:09 +0900

    [Bronze II] Title: 공 바꾸기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int arr[101];
+int n = 0;
+int m = 0;
+int i, j, temp;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n >> m;
+
+    for(int o = 0; o < n; o++) {
+        arr[o] = o + 1;
+    }
+
+    for(int o = 0; o < m; o++) {
+        cin >> i >> j;
+        swap(arr[i - 1], arr[j - 1]);
+    }
+
+    for(int o = 0; o < n; o++) {
+        cout << arr[o] << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
