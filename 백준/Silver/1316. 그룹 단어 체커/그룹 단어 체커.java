d87af221c629e6f5b502d018b83a7fc05c255c71
Lee <hjcloud@me.com>
2024-07-21 18:02:04 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -8,22 +8,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr;
-        int cnt = 0;
+        int cnt = 0; // 그룹 단어 개수
         for (int i = 0; i < n; i++) {
-            arr = new int[26];
-            char[] s = br.readLine().toCharArray();
-            char c = s[0];
-            boolean isSequence = true;
+            arr = new int[26]; // 알파벳 개수와 동일한 크기의 배열 선언
+            char[] s = br.readLine().toCharArray(); // 입력 문자를 문자 배열로 변환
+            char c = s[0]; // 직전 읽은 문자
+            boolean isGroupWord = true; // 그룹 단어 유무
             for (char value : s) {
-                if (value != c && arr[value - 'a'] != 0) {
-                    isSequence = false;
-                    break;
-                } else {
+                if (value == c || arr[value - 'a'] == 0) {
                     c = value;
                     arr[value - 'a']++;
+                    continue;
                 }
+                isGroupWord = false;
             }
-            if (isSequence) cnt++;
+            if (isGroupWord) cnt++;
         }
         System.out.println(cnt);
     }
