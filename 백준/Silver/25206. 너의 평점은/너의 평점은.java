32a28c8d5be2c34e94beaad7bf69834a84d0a7f9
Lee <hjcloud@me.com>
2024-07-21 20:11:27 +0900

    [Silver V] Title: 너의 평점은, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -3,10 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Map<String, Double> map = getGradeMap();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        double totalCredit = 0.0;
+        double creditMultiplyGPA = 0.0;
+        for (int i = 0; i < 20; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            st.nextToken();
+            double credit = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
+            if (map.get(grade) == null) continue;
+            totalCredit += credit;
+            creditMultiplyGPA += credit * map.get(grade);
+        }
+        System.out.println(creditMultiplyGPA / totalCredit);
+    }
+    
+    private static Map<String, Double> getGradeMap() {
         Map<String, Double> map = new HashMap<>();
         map.put("A+", 4.5);
         map.put("A0", 4.0);
@@ -17,22 +35,6 @@ public class Main {
         map.put("D+", 1.5);
         map.put("D0", 1.0);
         map.put("F", 0.0);
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        double totalCredit = 0.0;
-        double creditMultiplyGPA = 0.0;
-        for (int i = 0; i < 20; i++) {
-            String[] transcript = br.readLine().split(" ");
-            double credit = Double.parseDouble(transcript[1]);
-            String grade = transcript[2];
-            if (map.get(grade) == null) continue;
-            totalCredit += credit;
-            creditMultiplyGPA += credit * map.get(grade);
-        }
-
-        if (totalCredit == 0) {
-            System.out.println(0.0000);
-        } else {
-            System.out.println(creditMultiplyGPA / totalCredit);
-        }
+        return map;
     }
 }
