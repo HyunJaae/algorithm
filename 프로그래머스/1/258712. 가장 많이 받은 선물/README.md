d9056e596b1066114fce493f05c21cd809b64107
Lee <hjcloud@me.com>
2024-07-23 16:37:18 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 24.00 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level 1] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=java) 
 
 ### 성능 요약
 
-메모리: 137 MB, 시간: 150.29 ms
+메모리: 106 MB, 시간: 24.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 23일 15:58:23
+2024년 07월 23일 16:37:14
 
 ### 문제 설명
 
--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -2,51 +2,30 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        int answer = 0;
-
-        int[] giftRate = new int[friends.length]; // 선물 지수
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < friends.length; i++) {
-            int give = 0;
-            int take = 0;
-            for (String gift : gifts) {
-                String[] rel = gift.split(" ");
-                String friend = friends[i];
-                if (rel[0].equals(friend)) give++;
-                if (rel[1].equals(friend)) take++;
-            }
-            giftRate[i] = give - take;
+            map.put(friends[i], i);
+        }
+        int[] index = new int[friends.length];
+        int[][] record = new int[friends.length][friends.length];
+
+        for (String str : gifts) {
+            String[] cur = str.split(" ");
+            index[map.get(cur[0])]++;
+            index[map.get(cur[1])]--;
+            record[map.get(cur[0])][map.get(cur[1])]++;
         }
 
-        int[][] giveAndTake = new int[friends.length][friends.length];
-        for (String gift : gifts) {
-            String[] rel = gift.split(" ");
-            int row = 0;
-            int col = 0;
-            for (int j = 0; j < friends.length; j++) {
-                if (rel[0].equals(friends[j])) row = j;
-                if (rel[1].equals(friends[j])) col = j;
-            }
-            giveAndTake[row][col]++;
-        }
-
-        int[] takeGift = new int[friends.length];
-        for (int i = 0; i < friends.length; i++) {
-            for (int j = i; j < friends.length; j++) {
-                if (i == j) continue;
-                int give = giveAndTake[i][j];
-                int take = giveAndTake[j][i];
-
-                if (give > take) takeGift[i]++;
-                else if (give < take) takeGift[j]++;
-                else {
-                    if (giftRate[i] > giftRate[j]) takeGift[i]++;
-                    else if (giftRate[i] < giftRate[j]) takeGift[j]++;
-                }
-            }
-        }
-        Arrays.sort(takeGift);
-        
-        answer = takeGift[friends.length - 1];
-        return answer;
+       int ans = 0;
+       for (int i = 0; i < friends.length; i++) {
+           int cnt = 0;
+           for (int j = 0; j < friends.length; j++) {
+               if(i == j) continue;
+               if (record[i][j] > record[j][i]) cnt++;
+               else if (record[i][j] == record[j][i] && index[i] > index[j]) cnt++; 
+           }
+           ans = Math.max(cnt, ans);
+       }
+        return ans;
     }
 }
