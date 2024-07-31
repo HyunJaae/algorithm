2801b0ab47a5bcf0a783244ef0c0034d9aaaa356
Lee <hjcloud@me.com>
2024-07-31 13:53:06 +0900

    [level 3] Title: 이중우선순위큐, Time: 91.42 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,45 +2,26 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        int[] answer = new int[2];
         PriorityQueue<Integer> asc = new PriorityQueue<>();
         PriorityQueue<Integer> desc = new PriorityQueue<>(Collections.reverseOrder());
-        
-        for (int i = 0; i < operations.length; i++) {
-            String[] s = operations[i].split(" ");
-            String op = s[0];
-            int num = Integer.parseInt(s[1]);
-            if (op.equals("I")) {
+
+        for (String operation : operations) {
+            if (operation.contains("I")) {
+                int num = Integer.parseInt(operation.split(" ")[1]);
                 asc.offer(num);
                 desc.offer(num);
-            } else {
-                if (desc.size() > 0 && num == 1) {
-                    int d = desc.poll();
-                    Iterator<Integer> it = asc.iterator();
-                    while(it.hasNext()) {
-                        if (it.next() == d) {
-                            it.remove();
-                            break;
-                        }
-                    }
-                }
-                if (asc.size() > 0 && num == -1) {
-                    int a = asc.poll();
-                    Iterator<Integer> it = desc.iterator();
-                    while(it.hasNext()) {
-                        if (it.next() == a) {
-                            it.remove();
-                            break;
-                        }
-                    }
-                }
+                continue;
+            }
+            if (!desc.isEmpty() && operation.equals("D 1")) {
+                asc.remove(desc.poll());
+            }
+            if (!asc.isEmpty() && operation.equals("D -1")) {
+                desc.remove(asc.poll());
             }
         }
-        if (desc.size() == 0) {
+        if (desc.isEmpty() || asc.isEmpty()) {
             return new int[]{0, 0};
         }
-        answer[0] = desc.peek();
-        answer[1] = asc.peek();
-        return answer;
+        return new int[]{desc.peek(), asc.peek()};
     }
 }
