e8b7fdf83ee930e60455b0dc26c09b6fd97dc33b
Lee <hjcloud@me.com>
2024-08-07 22:05:19 +0900

    [level 2] Title: 모음 사전, Time: 5.82 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 77.7 MB, 시간: 70.78 ms
+메모리: 74.7 MB, 시간: 5.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 06일 23:49:28
+2024년 08월 07일 22:05:12
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,26 +1,30 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 class Solution {
     
-    private List<String> list = new ArrayList<>();
+    private Map<String, Integer> map = new HashMap<>();
+    private int index = 0;
     private boolean isWord = false;
     
     
     public int solution(String word) {
+        String[] vowels = {"A", "E", "I", "O", "U"};
 
-        insert("", word);
+        insert(vowels, "", word);
 
-        return list.indexOf(word);
+        return map.get(word);
     }
     
-    public void insert(String vowel, String word) {
-        if (vowel.length() > 5 || isWord) return;
-        list.add(vowel);
+    public void insert(String[] vowels, String vowel, String word) {
+        map.put(vowel, index++);
         if (vowel.equals(word)) {
             isWord = true;
             return;
         }
-        for (int i = 0; i < 5; i++) insert(vowel + "AEIOU".charAt(i), word);
+        if (vowel.length() == 5 || isWord) return;
+        for (int i = 0; i < 5; i++) {
+            insert(vowels, vowel + vowels[i], word);
+        }
     }
 }
