346483d2353785a1c2d8863ad588204a4c20066b
Lee <hjcloud@me.com>
2024-08-09 17:45:57 +0900

    [level 2] Title: 구명보트, Time: 12.63 ms, Memory: 56.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -5,21 +5,14 @@ class Solution {
         if (people.length == 1) return 1;
         int answer = 0;
         Arrays.sort(people);
-        int first = 0;
-        int second = people.length - 1;
+        int start = 0;
+        int end = people.length - 1;
         
-        while (first < second) {
-            int a = people[first];
-            int b = people[second];
-            if (a + b <= limit) {
-                answer++;
-                first++;
-                second--;
-            } else {
-                answer++;
-                second--;
-            }
-            if (first == second) answer++;
+        while (start < end) {
+            answer++;
+            if (people[start] + people[end] <= limit) start++;
+            end--;
+            if (start == end) answer++;
         }
         return answer;
     }
