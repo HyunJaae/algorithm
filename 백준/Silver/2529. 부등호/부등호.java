f7465cc5b38e9fbfebcf3ca3a9cd1d919b9d1790
Lee <hjcloud@me.com>
2024-08-28 13:48:07 +0900

    [Silver I] Title: 부등호, Time: 192 ms, Memory: 22456 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -1,53 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static int N;
-    private static final char[] arr = new char[10]; // 부등호는 최대 9개임
-    private static final boolean[] visited = new boolean[10]; // 0~9까지 check
-    private static final List<String> ans = new ArrayList<>();
+    static int k;
+    static boolean[] visited = new boolean[10];
+    static char[] arr;
+    static List<String> list =  new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        k = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
+        arr = new char[k];
+
+        for (int i = 0; i < k; i++) {
             arr[i] = st.nextToken().charAt(0);
-
-        dfs("", 0);
-        Collections.sort(ans);
-
-        System.out.println(ans.get(ans.size() - 1));
-        System.out.println(ans.get(0));
-        br.close();
+        }
+        dfs(0, "");
+        System.out.println(list.get(list.size() - 1));
+        System.out.println(list.get(0));
     }
 
-    private static void dfs(String num, int idx) {
-        if (idx == N + 1) {
-            ans.add(num);
+    private static void dfs(int i, String result) {
+        if (i == k + 1) {
+            list.add(result);
             return;
         }
 
-        for (int i = 0; i <= 9; i++) {
-            if (visited[i]) continue;
-            if (idx == 0 || ck(Character.getNumericValue(num.charAt(idx - 1)), i, arr[idx - 1])) {
-                visited[i] = true;
-                dfs(num + i, idx + 1);
-                visited[i] = false;
+        for (int j = 0; j < 10; j++) {
+            if (visited[j]) continue;
+            if (i == 0 || checkedNumber(i, j, result)) {
+                visited[j] = true;
+                dfs(i + 1, result + j);
+                visited[j] = false;
             }
         }
     }
 
-    private static boolean ck(int a, int b, char c) {
-        if (c == '<') {
-            return a <= b;
-        } else if (c == '>') {
-            return a >= b;
+    private static boolean checkedNumber(int i, int j, String result) {
+        int current = result.charAt(i - 1) - '0';
+        if (arr[i - 1] == '>') {
+            return current > j;
+        } else {
+            return current < j;
         }
-        return true;
     }
 }
