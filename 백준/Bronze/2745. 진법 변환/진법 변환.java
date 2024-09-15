b75b7156dd21cd12cc62481e0654f30029a360a0
Lee <hjcloud@me.com>
2024-09-15 13:56:26 +0900

    [Bronze II] Title: 진법 변환, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        String n = st.nextToken();
+        int b = Integer.parseInt(st.nextToken());
+        int res = 0;
+        int s = 0;
+        for (int i = n.length() - 1; i >= 0; i--) {
+            char c = n.charAt(i);
+            int v = 'A' <= c && c <= 'Z' ? c - 'A' + 10 : c - '0';
+            res += (int) (v * Math.pow(b, s++));
+        }
+
+        System.out.println(res);
+
+    }
+}
