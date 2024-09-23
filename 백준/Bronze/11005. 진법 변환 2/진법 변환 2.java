6e0c8346730634c226c654cd82fd84ae9dd86f45
Lee <hjcloud@me.com>
2024-09-23 09:14:47 +0900

    [Bronze I] Title: 진법 변환 2, Time: 100 ms, Memory: 14368 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,35 +9,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(st.nextToken()); // 10진법 수
-        int B = Integer.parseInt(st.nextToken()); // B 진법
+        int n = Integer.parseInt(st.nextToken()); // 10진법 수
+        int b = Integer.parseInt(st.nextToken()); // b 진법
 
-        Deque<Integer> stack = new ArrayDeque<>();
-        int q = N; // 몫
-        while (q >= B) {
-            int r = q % B; // 나머지
-            stack.push(r);
-            q /= B;
-        }
-
-        if (q > 9) {
-            char c = (char) (q - 10 + 'A');
-            sb.append(c);
-        } else {
-            sb.append(q);
-        }
-
-        int size = stack.size();
-        for (int i = 0; i < size; i++) {
-            int s = stack.pop();
-            if (s > 9) {
-                char c = (char) (s - 10 + 'A');
-                sb.append(c);
+        while (n > 0) {
+            int r = n % b; // 나머지
+            if (r < 10) {
+                sb.append(r);
             } else {
-                sb.append(s);
+                char c = (char) (r + 55);
+                sb.append(c);
             }
+            n /= b;
         }
 
-        System.out.println(sb);
+        System.out.println(sb.reverse());
     }
 }
