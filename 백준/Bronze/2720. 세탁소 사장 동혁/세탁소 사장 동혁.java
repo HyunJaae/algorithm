af17703e4d73d049f09cbd8cb58fe4ff03525366
Lee <hjcloud@me.com>
2024-09-24 09:34:54 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 100 ms, Memory: 14016 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -7,18 +7,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
+        int[] arr = {25, 10, 5, 1};
 
         StringBuilder sb = new StringBuilder();
         int c;
         for (int i = 0; i < t; i++) {
             c = Integer.parseInt(br.readLine());
-            sb.append(c / 25).append(" ");
-            c = c % 25;
-            sb.append(c / 10).append(" ");
-            c = c % 10;
-            sb.append(c / 5).append(" ");
-            c = c % 5;
-            sb.append(c).append("\n");
+            for (int k : arr) {
+                sb.append(c / k).append(" ");
+                c = c % k;
+            }
+            sb.append("\n");
         }
 
         System.out.println(sb);
