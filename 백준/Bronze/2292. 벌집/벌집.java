ec9cdfc7e72255f71c7caac9299e3079c6aa70fa
Lee <hjcloud@me.com>
2024-10-01 18:47:59 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -7,12 +7,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int cnt = 1;
         int k = 1;
         while (k < n) {
-            cnt++;
             k += cnt * 6;
+            cnt++;
         }
-        System.out.println(cnt + 1);
+        System.out.println(cnt);
     }
 }
